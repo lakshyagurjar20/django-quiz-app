b022a6e52713a678f156009608482bccfa876541
lakshyagurjar20 <lakshygurjar2095@gmail.com>
2025-06-28 12:54:04 +0530

    Add Question model with options and admin registration

--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    option1 = models.CharField(max_length=255)
+    option2 = models.CharField(max_length=255)
+    option3 = models.CharField(max_length=255)
+    option4 = models.CharField(max_length=255)
+    correct_option = models.IntegerField(choices=[
+        (1, 'Option 1'),
+        (2, 'Option 2'),
+        (3, 'Option 3'),
+        (4, 'Option 4'),
+    ])
+
+    def __str__(self):
+        return self.question_text
